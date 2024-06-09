ed701b3887b45d18289a790a3d294aacffe40a47
Myron <josephonyekachi77@gmail.com>
2024-06-09 02:25:00 +0100

    updated

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,62 @@
-export const signup = (req, res) => {
-    console.log("signup Route")
-    res.send('signup Route')
-} 
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
+
+export const signup = async (req, res) => {
+
+    try {
+        const { fullName, username, password, confirmPassword, gender } = req.body
+
+        if (password !== confirmPassword) {
+            return res.status(400).json({ error: "Passwords does not match" })
+        }
+
+        const user = await User.findOne({ username });
+
+        if (user) {
+            return res.status(400).json({ error: "User already exists" })
+        }
+
+        //Hash Password Here
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        //Choose dynamic profile picture
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        const newUser = new User({
+            fullName,
+            username,
+            password: hashedPassword,
+            gender,
+            profilePic: gender === 'male' ? boyProfilePic : girlProfilePic
+        })
+
+        if (newUser) {
+            //Generate JWT token here
+            generateTokenAndSetCookie(newUser._id, res)
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic,
+            })
+        } else {
+            res.status(400).json({error: "Invalid user data"})
+        }
+
+    } catch (error) {
+        console.log("Error in signup controller", error.message)
+        res.status(500).json({ error: "Internal Server Error" })
+    }
+}
+
 export const login = (req, res) => {
     console.log("Login Route")
     res.send('Login Route')
-} 
+}
 export const logout = (req, res) => {
     console.log("Logout Route")
     res.send('Logout Route')
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,27 @@
 import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "../backend/routes/auth.routes.js"
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express()
-
-dotenv.config();
 const PORT = process.env.PORT || 5000
 
-app.get("/", (req, res) => {
-    //Root Route http://localhost:5000
-    res.send("Hello World!!")
-})
+dotenv.config();
+
+//to pass incoming request with json payload from the req.body
+app.use(express.json());
 
 //(1) To manage the routes, we have to create the route file
 app.use("/api/auth", authRoutes)
 
-app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
+// app.get("/", (req, res) => {
+//     //Root Route http://localhost:5000
+//     res.send("Hello World!!")
+// })
+
+
+
+app.listen(PORT, () => {
+    connectToMongoDB()
+    console.log(`Server Running on port ${PORT}`)
+});

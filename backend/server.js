6f81050ae34efd456357bd998a7c4523109d7ef8
Myron <josephonyekachi77@gmail.com>
2024-08-04 02:02:49 +0100

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
@@ -9,6 +10,8 @@ import userRoutes from "../backend/routes/user.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
+
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000
 
@@ -22,6 +25,13 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
+
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // app.get("/", (req, res) => {
 //     //Root Route http://localhost:5000
